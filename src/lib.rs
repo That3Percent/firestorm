146b41107c80948d4236d55e03d2fb956ae90567
Zac Burns <That3Percent@gmail.com>
2020-06-20 17:31:35 -0500

    Render non-merged flame-chart

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ use std::collections::HashMap;
 #[cfg(feature = "enabled")]
 use std::fmt;
 #[cfg(feature = "enabled")]
-use std::time::Instant;
+use std::time::{Duration, Instant};
 
 // TODO: Feature to enable, so that all crates that use it are consistently enabled
 
@@ -61,6 +61,9 @@ impl fmt::Display for FmtStr {
 enum Event {
     Start { time: Instant, tag: FmtStr },
     End { time: Instant },
+    // TODO: Add Pause, Resume to help with things like the
+    // amortized cost of expanding the length of the events
+    // array getting reported as a part of another operation.
 }
 
 struct SpanGuard;
@@ -123,14 +126,31 @@ pub fn clear() {}
 
 #[cfg(feature = "enabled")]
 /// Convert events to the format that inferno is expecting
-fn lines() -> Vec<String> {
+fn lines(options: &Options) -> Vec<String> {
     with_events(|events| {
         struct Frame {
             name: String,
             start: Instant,
         }
+        struct Line {
+            name: String,
+            time: u128,
+        }
+
+        fn push_line(lines: &mut Vec<Line>, name: String, duration: Duration) {
+            let time = duration.as_nanos();
+            if let Some(prev) = lines.last_mut() {
+                if &prev.name == &name {
+                    prev.time += time;
+                    return;
+                }
+            }
+            lines.push(Line { name, time });
+        }
+
         let mut stack = Vec::<Frame>::new();
         let mut collapsed = HashMap::<_, u128>::new();
+        let mut lines = Vec::<Line>::new();
 
         for event in events.iter() {
             match event {
@@ -138,6 +158,9 @@ fn lines() -> Vec<String> {
                     let mut name = format!("{}", tag).replace(";", "").replace(" ", "_");
                     if let Some(parent) = stack.last() {
                         name = format!("{};{}", &parent.name, name);
+                        if !options.merge {
+                            push_line(&mut lines, name.clone(), *time - parent.start);
+                        }
                     }
                     let frame = Frame {
                         name: name,
@@ -147,41 +170,94 @@ fn lines() -> Vec<String> {
                 }
                 Event::End { time } => {
                     let Frame { name, start } = stack.pop().unwrap();
-                    let elapsed = (*time - start).as_nanos();
-                    let entry = collapsed.entry(name).or_default();
-                    *entry = entry.wrapping_add(elapsed);
-                    if let Some(parent) = stack.last() {
-                        let entry = collapsed.entry(parent.name.clone()).or_default();
-                        *entry = entry.wrapping_sub(elapsed);
+                    let elapsed = *time - start;
+                    if options.merge {
+                        let elapsed = elapsed.as_nanos();
+                        let entry = collapsed.entry(name).or_default();
+                        *entry = entry.wrapping_add(elapsed);
+                        if let Some(parent) = stack.last() {
+                            let entry = collapsed.entry(parent.name.clone()).or_default();
+                            *entry = entry.wrapping_sub(elapsed);
+                        }
+                    } else {
+                        push_line(&mut lines, name, elapsed);
+                        if let Some(parent) = stack.last_mut() {
+                            parent.start = *time;
+                        }
                     }
                 }
             }
         }
         assert!(stack.is_empty(), "Mimatched start/end");
 
-        //let mut collapsed: Vec<_> = collapsed.iter().collect();
-        //collapsed.sort_by_key(|(_, time)| *time);
+        fn format_line(name: &str, time: &u128) -> Option<String> {
+            if *time == 0 {
+                None
+            } else {
+                Some(format!("{} {}", name, time))
+            }
+        }
 
-        collapsed
-            .iter()
-            .map(|(name, time)| format!("{} {}", name, time))
-            .collect()
+        if options.merge {
+            collapsed
+                .iter()
+                .filter_map(|(name, time)| format_line(name, time))
+                .collect()
+        } else {
+            lines
+                .iter()
+                .filter_map(|Line { name, time }| format_line(name, time))
+                .collect()
+        }
     })
+}
+
+/// This API is unstable, and will likely go away
+#[derive(Default)]
+struct Options {
+    /// Merges all instances of the same stack into a single bar.
+    /// This may give a better overview of, for example, how much total
+    /// time a method took. But, will not retain information like how
+    /// many times a method was called.
+    pub merge: bool,
+    _priv: (),
 }
 
 /// Write the data to an svg
 #[cfg(feature = "enabled")]
-pub fn to_svg<W: std::io::Write, F: FnOnce() -> W>(f: F) -> Result<(), impl std::error::Error> {
-    let lines = lines();
+pub fn to_svg<W: std::io::Write>(writer: &mut W) -> Result<(), impl std::error::Error> {
+    let options = Default::default();
+
+    let lines = lines(&options);
+
+    /*
+    // Output lines for debugging
+    use std::io::Write;
+    let mut f = std::fs::File::create("C:\\git\\flames.txt").unwrap();
+    for line in lines.iter() {
+        f.write(line.as_bytes()).unwrap();
+        f.write("\n".as_bytes()).unwrap();
+    }
+    drop(f);
+    */
+
     let mut options = flamegraph::Options {
         count_name: "ns".to_owned(),
         hash: true,
+        flame_chart: !options.merge,
         ..Default::default()
     };
-    flamegraph::from_lines(&mut options, lines.iter().rev().map(|s| s.as_str()), f())
+    flamegraph::from_lines(&mut options, lines.iter().rev().map(|s| s.as_str()), writer)
 }
 
+#[inline(always)]
 #[cfg(not(feature = "enabled"))]
-pub fn to_svg<W: std::io::Write, F: FnOnce() -> W>(_f: F) -> Result<(), std::convert::Infallible> {
+pub fn to_svg<W: std::io::Write>(_writer: &mut W) -> Result<(), std::convert::Infallible> {
     Ok(())
+}
+
+/// Returns whether or not
+#[inline(always)]
+pub fn enabled() -> bool {
+    cfg!(feature = "enabled")
 }
