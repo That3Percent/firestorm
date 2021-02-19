94bff2087667409d4068a52d01367453afb34f13
Zac Burns <That3Percent@gmail.com>
2021-02-18 21:18:47 -0600

    Doc fixes

--- firestorm/src/lib.rs
+++ firestorm/src/lib.rs
@@ -1,5 +1,5 @@
 //! Firestorm is the low-overhead intrusive flamegraph profiler for Rust.
-//! For documentation and usage, please see https://github.com/That3Percent/firestorm
+//! For documentation and usage, please see [Firestorm on Github](https://github.com/That3Percent/firestorm)
 
 #[cfg(any(feature = "enable_cpu_time", feature = "enable_system_time"))]
 pub use firestorm_enabled::*;
